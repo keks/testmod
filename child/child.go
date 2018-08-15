728196c81e5460cdce129c24dad4062410f04e27
keks <keks@cryptoscope.co>
2018-08-15 12:03:31 +0200

    child: add IsLeaf

--- child/child.go
+++ child/child.go
@@ -1,3 +1,11 @@
 package child // import "github.com/keks/testmod/child"
 
+import (
+	"github.com/keks/testmod/leaf"
+)
+
 var IsChild = true
+
+func IsLeaf() bool {
+	return !leaf.IsLeaf()
+}
--- child/go.mod
+++ child/go.mod
@@ -1,1 +1,3 @@
 module github.com/keks/testmod/child
+
+require github.com/keks/testmod v0.0.0-20180815100117-dc249a067c0a
