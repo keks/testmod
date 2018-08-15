d9f3b6a8288f7480867bbacf051c2e2ea1a60f0e
keks <keks@cryptoscope.co>
2018-08-15 11:57:36 +0200

    add root

---
+++ root/go.mod
@@ -0,0 +1,3 @@
+module github.com/keks/testmod/root
+
+require github.com/keks/testmod/child v0.0.0-20180815095625-c37fa5e6ada6
---
+++ root/main.go
@@ -0,0 +1,11 @@
+package main // import "github.com/keks/testmod/root"
+
+import (
+	"fmt"
+	
+	"github.com/keks/testmod/child"
+)
+
+func main() {
+	fmt.Println("child is child:", child.IsChild)
+}
